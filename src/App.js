ef41fe6343620f2a1e049cf629291ac7a6274b37
EugeneKruglei <eugene787@yandex.ru>
2018-12-19 16:08:35 +0300

    First sliding problems

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Page from './page';
+import Nav from './nav';
+import Link from './link';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <div className="App">
+                <div>
+                    <Link to="/green" >
+                        <button>green</button>
+                    </Link>
+                    <Link to="/red" >
+                        <button>red</button>
+                    </Link>
+                    <Link to="/blue" >
+                        <button>blue</button>
+                    </Link>
+                    <Link to="/brown" >
+                        <button>brown</button>
+                    </Link>
+                </div>
+                <Nav>
+                    <Page path="/green">
+                        <div className="page" style={{ backgroundColor: 'green' }}>
+                            bla1
+                        </div>
+                    </Page>
+                    <Page path="/red">
+                        <div className="page" style={{ backgroundColor: 'red' }}>
+                            bla1
+                        </div>
+                    </Page>
+                    <Page path="/blue">
+                        <div className="page" style={{ backgroundColor: 'blue' }}>
+                            bla1
+                        </div>
+                    </Page>
+                    <Page path="/brown">
+                        <div className="page" style={{ backgroundColor: 'brown' }}>
+                            bla1
+                        </div>
+                    </Page>
+                </Nav>
+
+            </div>
+        );
+    }
 }
 
 export default App;
